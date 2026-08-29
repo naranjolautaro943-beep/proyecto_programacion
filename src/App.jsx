ca5692e20b74cef766b1b40f62683413d045f66d
Emiliano Lopez <emilianolopeez42@.com>
2026-08-29 16:22:54 -0300

    Falta agregar un bloque del finalizar venta

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Footer from './Components/Cards/Footer/Footer'
 import Navbar from './Components/Cards/Navbar/Navbar'
 import Sidebar from './Components/Cards/Sidebar/Sidebar'
 import Main from './Components/main/Main'
+import './App.css'
 
 function App() {
   
