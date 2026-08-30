455f9d05597c36ccda14050a83f4d7c4445875f5
unknown <lautaro naranjo>
2026-08-30 00:08:17 -0300

    arreglando header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import Footer from './Components/Cards/Footer/Footer'
-import Navbar from './Components/Cards/Navbar/Navbar'
 import Sidebar from './Components/Cards/Sidebar/Sidebar'
 import Main from './Components/main/Main'
 import { useState } from 'react'
-
+import Header from './Components/Cards/Header/Header'
 function App() {
   const [abierto, setAbierto] = useState(false);
   return (
     <>
-    <Navbar/>
+    <Header/>
     <Sidebar abierto={abierto} setAbierto={setAbierto}/>
     <Main  abierto={abierto} />
     <Footer/>
