41ad02f78ffd52ab9a9688c5af58c1e23088ec20
unknown <lautaro naranjo>
2026-08-30 19:08:39 -0300

    termine de configurar el sidebar

--- src/Components/Cards/Sidebar/Sidebar.jsx
+++ src/Components/Cards/Sidebar/Sidebar.jsx
@@ -5,22 +5,21 @@ export default function Sidebar({ abierto, setAbierto }) {
   const anchoSidebar = 280;
     return (
       <div className="d-flex">
-      <button className="btn btn-dark m-2" onClick={() => setAbierto(!abierto)} style={{ position: 'fixed' ,top:12, left:12, zIndex:1050 }}>
-        ☰
-      </button>
-
+        <div boton-opciones>
+          <button className="btn btn-dark m-2" onClick={() => setAbierto(!abierto)} style={{ position: 'fixed' ,top:12, left:12, zIndex:1050 }}>
+            ☰
+          </button>
+        </div>
       {abierto && (
-        <div className="d-flex flex-column flex-shrink-0 p-3 sidebar" style={{ position:'fixed',top:0,left:0,width:anchoSidebar, height:'100vh',transform:abierto? 'translateX(0)' : 'translateX(-100%)', transition:'transform 0.3s ease-in-out', }}>
-          <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <svg className="bi pe-none me-2" width="40" height="32" viewBox="0 0 118 94" fill="currentColor" aria-hidden="true">
-              <path fillRule="evenodd" clipRule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z" />
-            </svg>
-            <span className="fs-4">Sidebar</span>
+        <div className="d-flex flex-column  p-3 sidebar" style={{ position:'fixed',top:0,left:0,width:anchoSidebar, height:'100vh',transform:abierto? 'translateX(0)' : 'translateX(-100%)', transition:'transform 0.3s ease-in-out', }}>
+          <a className="text-navbar">
+            <img src=''/>
+            <span className="titulo-sidebar">Sidebar</span>
           </a>
           <hr />
-          <ul className="nav nav-pills flex-column mb-auto">
+          <ul className="nav nav-pills flex-column ">
             <li className="nav-item">
-              <a href="#" className="nav-link active" aria-current="page">
+              <a href="#" className="nav-link active">
                 <svg className="bi pe-none me-2" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true">
                   <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
                 </svg>
@@ -28,7 +27,7 @@ export default function Sidebar({ abierto, setAbierto }) {
               </a>
             </li>
             <li>
-              <a href="#" className="nav-link text-white">
+              <a className="nav-link text-white">
                 <svg className="bi pe-none me-2" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true">
                   <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.258l3.434-4.297a.389.389 0 0 0-.029-.518z" />
                   <path fillRule="evenodd" d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.397 12.533 6.358 12 8 12s3.604.532 4.923.96c.757.245 1.477-.056 1.68-.631A7 7 0 0 0 8 3z" />
