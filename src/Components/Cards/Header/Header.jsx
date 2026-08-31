d41588d27f433e0774efbf0bf327f2ed4c1f22ac
unknown <lautaro naranjo>
2026-08-30 22:14:09 -0300

    acomode el header

--- src/Components/Cards/Header/Header.jsx
+++ src/Components/Cards/Header/Header.jsx
@@ -3,16 +3,13 @@ import './Header.css'
 export default function Header() {
     return (
             <header className="header">
-      
-        <div className='header-titulo'>
-            <h1>Kiosco Azul</h1>
-        </div>
-      
-
-      <div className="header-lado header-derecha">
-        <span className="header-fecha">31 de Agosto , 2026</span>
-      </div>
-    </header>
+                <div className='header-titulo'>
+                    <h1>Kiosco Azul</h1>
+                </div>
+                <div className="header-lado">
+                    <span className="header-fecha">31 de Agosto , 2026</span>
+                </div>
+            </header>
         
         
     )
